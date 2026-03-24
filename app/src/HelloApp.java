95b335d3aff5f5ecc48a2482fb4733433af22d94
Shourya <parasharshourya07@gmail.com>
2026-03-25 01:09:06 +0530

    UC1: Display Hello World

---
+++ app/src/HelloApp.java
@@ -0,0 +1,15 @@
+/**
+ * HelloApp UC1 - Hello World Display Application
+ *
+ * This class demonstrates a simple Java application that displays "Hello World"
+ * to the console.
+ *
+ * @author Shourya Parashar
+ * @version 1.0
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
